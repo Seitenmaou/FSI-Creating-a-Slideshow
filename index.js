1704b38aed814a0e9be9e9390ab9aa267d5debcc
Euri Tanaka <97496667+Seitenmaou@users.noreply.github.com>
2022-04-23 16:48:09 -0700

    added button functionality

--- index.js
+++ index.js
@@ -6,3 +6,80 @@ const images = [
     './assets/5.jpg',
     './assets/6.jpg'
 ]
+let imageIndex = 0;
+function setImage(imageIndex){
+    document.querySelector('.image').src = images[imageIndex];
+}
+
+function nextImage(positive){
+    if (positive){
+        if (imageIndex < images.length - 1){
+            imageIndex++
+        }
+        else {
+            imageIndex = 0
+        }
+    } else {
+        if (imageIndex > 0){
+            imageIndex--
+        }
+        else {
+            imageIndex = images.length - 1
+        }
+    }
+    setImage(imageIndex)
+    for (totalButtons = 0; totalButtons < buttons.length; totalButtons++){
+        buttons[totalButtons].textContent = 'x'
+    }
+    buttons[imageIndex].textContent = '○'
+}
+
+let button0 = document.getElementById('button-0');
+button0.addEventListener('click', () => {
+    imageIndex = 0;
+    setImage(imageIndex)
+})
+
+let buttonID = 'button-'
+let buttons = document.getElementsByClassName('bubble')
+for (let currentButton = 0; currentButton < buttons.length; currentButton++){
+    buttonID = `button-${currentButton}`
+    let addButton = document.getElementById(buttonID)
+    addButton.addEventListener('click', () => {
+        imageIndex = currentButton;
+        for (totalButtons = 0; totalButtons < buttons.length; totalButtons++){
+            buttons[totalButtons].textContent = 'x'
+        }
+        buttons[currentButton].textContent = '○'
+    
+
+        setImage(imageIndex)
+    })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let next = document.getElementsByClassName('next side-bar');
+next[0].addEventListener('click', () => {
+    nextImage(true)
+    
+})
+let previous = document.getElementsByClassName('previous side-bar');
+previous[0].addEventListener('click', () => {
+    nextImage(false)
+})
+
+setImage(imageIndex)
